5a59383b345e5a8c3bde70184c4302d91844dc6d
Bhartendu Jouhari <Bhartendu.Jouhari@genmills.com>
2021-02-10 17:24:44 +0530

    First Check-In

---
+++ components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>Footer</p>
+      </div>
+    <div class="column has-text-right">
+      <p>Vuemmerce</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: #b9e2fc;
+   color: black;
+ }
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+           Vue.js projects
+        </h1>
+        <h2 class="subtitle">
+        Created by Ravinder and Bhartendu
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background: rgb(81, 186, 252);
+    }
+    h1, h2 {
+      color: #fff;
+    }
+</style>
